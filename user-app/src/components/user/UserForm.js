37e2db450ae2452e85fd88e2ebb07b1884db20f9
smita <smitamurawat@gmail.com>
2022-07-14 00:47:38 +0530

    Added form validations with error modal component

--- user-app/src/components/user/UserForm.js
+++ user-app/src/components/user/UserForm.js
@@ -1,14 +1,31 @@
 import React, { useState } from 'react';
 import Button from '../UI/Button';
+import ErrorModal from '../UI/ErrorModal';
 
 function UserForm(props) {
     const [name, SetuserName] = useState('');
     const [age, SetAge] = useState('');
+    const [error, SetError] = useState();
 
     const userNameHandler = (event) => SetuserName(event.target.value);
     const AgeHandler = (event) => SetAge(event.target.value);
     const FormHandler = (e) => {
         e.preventDefault();
+        if (name.trim().length === 0 || age.trim().length === 0) {
+            SetError({
+                title: "Invalid input",
+                message: "Input field required some values!!"
+            });
+            return;
+        }
+        if (+age < 1){
+            SetError({
+                title: "Invalid age",
+                message: "Age should be greater than 1 !!!"
+            })
+            return;
+        }
+
         const formData = {
 
             username: name,
@@ -21,21 +38,33 @@ function UserForm(props) {
 
 
     };
+
+    const errorHandler=()=>{
+        SetError(null);
+    }
     return (
+        <div>
+            {error && (
+                <ErrorModal
+                    title={error.title}
+                    message={error.message}
+                    onConfirm={errorHandler}
+                />
+            )}
+            <form onSubmit={FormHandler}>
 
-        <form onSubmit={FormHandler}>
-
-            <label>UserName</label>
-            <input type="text" value={name} onChange={userNameHandler}></input>
+                <label>UserName</label>
+                <input type="text" value={name} onChange={userNameHandler}></input>
 
 
-            <label>Age(years)</label>
-            <input type="number" value={age} onChange={AgeHandler}></input>
+                <label>Age(years)</label>
+                <input type="number" value={age} onChange={AgeHandler}></input>
 
 
-            <Button type="submit">Add User</Button>
+                <Button type="submit">Add User</Button>
 
-        </form>
+            </form>
+        </div>
 
     );
 
